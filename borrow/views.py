b8dd167a5c8d024d1484a64502f72752e714462e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-06-23 17:26:36 +0600

    mission accomplished 🔥🚀🔥

--- borrow/views.py
+++ borrow/views.py
@@ -7,17 +7,16 @@ from django.contrib.auth.decorators import login_required
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+def send_borrow_email(user_account, borrow, subject, template):
+    message = render_to_string(template, {
+        'user_account': user_account,
+        'borrow': borrow,
+    })
+    send_email = EmailMultiAlternatives(subject, '', to=[user_account.user.email])
+    send_email.attach_alternative(message, "text/html")
+    send_email.send()
 
-
-def send_borrow_email(user_account,borrow, subject, template):
-        message = render_to_string(template, {
-            'user_account' : user_account,
-            'borrow' : borrow,
-        })
-        send_email = EmailMultiAlternatives(subject, '', to=[user_account.user.email])
-        send_email.attach_alternative(message, "text/html")
-        send_email.send()
-@login_required
+@login_required(login_url='/user_account/login/')
 def borrow_book(request, id):
     if request.method == 'POST':
         book = get_object_or_404(Books, pk=id)
@@ -35,7 +34,7 @@ def borrow_book(request, id):
     else:
         # Handle GET request (optional: add additional logic for viewing the borrow form)
         user_account = UserAccount.objects.get(user=request.user)
-        gender=user_account.gender
-        print(gender,"checking")
+        gender = user_account.gender
+        print(gender, "checking")
         book = get_object_or_404(Books, pk=id)
-        return render(request, 'book_detail.html', {'gender':gender,'book': book})
+        return render(request, 'book_detail.html', {'gender': gender, 'book': book})
--- library_management/settings.py
+++ library_management/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+LOGIN_URL = '/user_account/login/'
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://library-management-i2ug.onrender.com', 'https://*.127.0.0.1']
